d9afe0b958de7737e0b552ec1c2a42669bde0da0
shubhpatel2197 <202151111@iiitvadodara.ac.in>
2024-04-06 23:46:13 +0530

    header

---
+++ Frontend/speakingclub/src/components/Header/index.jsx
@@ -0,0 +1,55 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+
+function Header() {
+  return (
+    <Navbar data-bs-theme="dark" expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav bg="dark"
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px',backgroundColor: 'primary' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Link</Nav.Link>
+            <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#" disabled>
+              Link
+            </Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2 "
+              aria-label="Search"
+              style={{width:'30vw'}}
+            />
+            <Button className="mx-2" variant="outline-success">Search</Button>
+          </Form>
+          <Button variant="outline-success">Login</Button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
