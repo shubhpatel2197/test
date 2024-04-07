0fe15bd7aa238a72b100c676718587818dba36e5
raj-j-shah <rajcoder19@gmail.com>
2024-04-07 10:36:09 +0530

    groups added

--- Frontend/speakingclub/src/App.js
+++ Frontend/speakingclub/src/App.js
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import { useState } from 'react';
-import CreateGroup from './components/Create_group/CreateGroup';
+import CreateGroup from './components/Create_group/CreateGroup';import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from './components/Groups';
 
 function App() {
 
@@ -18,10 +20,60 @@ function App() {
 
   console.log(backdrop);
 
+  const [groups, setGroups] = useState([
+    {
+      maxLimit: 5,
+      avatar: 'h',
+      name: 'user',
+      language: 'Hindi + English',
+      level: 'advanced',
+      info: 'time-pass',
+      id: 1
+    },
+    {
+      maxLimit: 9,
+      avatar: 'h',
+      name: 'user',
+      language: 'Gujarati + English',
+      level: 'advanced',
+      info: 'time-pass',
+      id: 2
+    },
+    {
+      maxLimit: 8,
+      avatar: 'h',
+      name: 'user',
+      level: 'Beginner',
+      language: 'English',
+      info: 'Tofel',
+      id: 3
+    },
+    {
+      maxLimit: 15,
+      avatar: 'h',
+      name: 'user',
+      level: 'advanced',
+      info: 'IELTS',
+      language: 'Korean + English',
+      id: 4
+    },
+  ]);
+
   return (
     <>
-     <Header setbackon={setbackon}/>
+       <Header setbackon={setbackon} />
     <CreateGroup setbackoff={setbackoff} backdrop={backdrop}></CreateGroup>
+      <div className='container' style={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-between'
+      }}>
+        {
+          groups.map((group) => {
+            return <Card key={group.id} data={group}/>;
+          })
+        }
+      </div>
     </>
   );
 }
---
+++ Frontend/speakingclub/src/components/Groups/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export default function Card({ data }) {
+  const renderCircles = () => {
+    const circles = [];
+    for (let i = 0; i < data.maxLimit; i++) {
+      circles.push(<div key={i} className="circle" style={circleStyle}></div>);
+    }
+    return circles;
+  };
+
+  const circleStyle = {
+    width: '50px',
+    height: '50px',
+    border: '1px dotted white',
+
+    borderRadius: '50%',
+    display: 'inline-block',
+    margin: '5px',
+  };
+
+  const circlesContainerStyle = {
+    marginBottom: '10px', /* Adjust as needed */
+  };
+
+  return (
+    <div className="card text-bg-dark" style={{ width: '40rem', marginTop: '20px' }}>
+      <div className="card-body">
+        <h5 className="card-title">{data.language} ({data.level})</h5>
+        <p className="card-text">{data.info} </p>
+        <div className="circles-container" style={circlesContainerStyle}>{renderCircles()}</div>
+        <div style={{top:'40px', display:'flex', flexWrap:'wrap',justifyContent:'flex-end', alignContent:'flex-end',}}>
+        <a  className="btn btn-primary" >Join group</a>
+        </div>
+      </div>
+    </div>
+  );
+}
