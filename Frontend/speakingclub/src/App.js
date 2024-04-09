a713dc1a4fdc6c68f58b5ae767235573169f892d
raj-j-shah <rajcoder19@gmail.com>
2024-04-09 17:34:57 +0530

    Connected with backend

--- Frontend/speakingclub/src/App.js
+++ Frontend/speakingclub/src/App.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import CreateGroup from './components/Create_group/CreateGroup';import 'bootstrap/dist/css/bootstrap.min.css';
-import Card from './components/Groups';
+import LoadGroup from './components/Loadgroups';
 
 function App() {
   
@@ -13,66 +13,17 @@ function App() {
   }
 
   function setbackon(){
-    console.log(backdrop);
     setbackdrop(true);
   }
 
-  console.log(backdrop);
 
-  const [groups, setGroups] = useState([
-    {
-      maxLimit: 5,
-      avatar: 'h',
-      name: 'user',
-      language: 'Hindi + English',
-      level: 'advanced',
-      info: 'time-pass',
-      id: 1
-    },
-    {
-      maxLimit: 9,
-      avatar: 'h',
-      name: 'user',
-      language: 'Gujarati + English',
-      level: 'advanced',
-      info: 'time-pass',
-      id: 2
-    },
-    {
-      maxLimit: 8,
-      avatar: 'h',
-      name: 'user',
-      level: 'Beginner',
-      language: 'English',
-      info: 'Tofel',
-      id: 3
-    },
-    {
-      maxLimit: 15,
-      avatar: 'h',
-      name: 'user',
-      level: 'advanced',
-      info: 'IELTS',
-      language: 'Korean + English',
-      id: 4
-    },
-  ]);
+  
 
   return (
     <>
        <Header setbackon={setbackon} />
     <CreateGroup setbackoff={setbackoff} backdrop={backdrop}></CreateGroup>
-      <div className='container' style={{
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'space-between'
-      }}>
-        {
-          groups.map((group) => {
-            return <Card key={group.id} data={group}/>;
-          })
-        }
-      </div>
+      <LoadGroup></LoadGroup>
     </>
   );
 }
---
+++ Frontend/speakingclub/src/components/Loadgroups/index.jsx
@@ -0,0 +1,38 @@
+import { useEffect,useState } from "react";
+import Card from "../Groups"
+export default function LoadGroup(){
+    const [groups, setGroups] = useState([]);
+  useEffect(()=>{
+    const fetcher = setInterval(()=>{
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow"
+    };
+    
+    fetch("http://localhost:4000/allgroups", requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        setGroups(result);
+    })
+      .catch((error) => console.error(error));
+
+  },5000);
+  return ()=>{
+    clearInterval(fetcher);
+  }
+  },[])
+    return (
+        <div className='container' style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'space-between'
+          }}>
+            {
+              groups.map((group) => {
+                return <Card key={group.id} data={group}/>;
+              })
+            }
+          </div>
+    )
+}
