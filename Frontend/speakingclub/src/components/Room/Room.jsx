f511ccf9f0a96a7b0cf0e479e7aee1d8ec4db2e7
Shubh Patel <98100054+shubhpatel2197@users.noreply.github.com>
2024-04-16 17:16:28 +0530

    Update Room.jsx

--- Frontend/speakingclub/src/components/Room/Room.jsx
+++ Frontend/speakingclub/src/components/Room/Room.jsx
@@ -46,7 +46,7 @@ export default function Room(props) {
 
     useEffect(()=>{
         
-        socketRef.current = io.connect('http://localhost:4000', { transports: ['websocket'] });
+        socketRef.current = io.connect('https://test-6-vdrh.onrender.com', { transports: ['websocket'] });
         socketRef.current.on('connect', () => {
             console.log('Connected to socket server');
             setSocketConnected(true);
