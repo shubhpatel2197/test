6f9ab4483630e856f3a719a0591548c5544a5ea7
Shubh Patel <98100054+shubhpatel2197@users.noreply.github.com>
2024-04-16 12:08:33 +0530

    Update controller.js

--- Backend/Db/controller.js
+++ Backend/Db/controller.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-dotenv.config();
+// const dotenv = require('dotenv');
+// dotenv.config();
 const url = "mongodb+srv://raj21032003:1234@cluster0.wgonutm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 function fire(){
     mongoose.connect(url)
